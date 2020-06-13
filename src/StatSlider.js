6daa5c5424d3d2b7952da437aea1a99d645ad4c1
Dadamuga Mazino <destructicus_lives@yahoo.com>
2020-06-13 14:52:21 +0100

    converted from CSS to sass
    Co-authored-by: Andrea Araujo <acsalvatierrar@gmail.com>

--- src/StatSlider.js
+++ src/StatSlider.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import Button from './Button';
 
-import  './StatSlider.css';
+import styles from './StatSlider.module.scss';
 
 function StatSlider({statName, maxValue = 10, minValue = 0}) {
 
     const [statValue, setStatValue] = useState(0);
     
     return (
-        <div className="stat-container">
+        <div className={styles.statContainer}>
             <Button onClick={() => { setStatValue(Math.max(statValue - 1, minValue)) }}>-</Button>
-            <p className="stat-txt">{statName}: {statValue}</p>
+            <p className={styles.statTxt}>{statName}: {statValue}</p>
             <Button onClick={() => { setStatValue(Math.min(statValue + 1, maxValue)) }}>+</Button>
         </div>
     )
